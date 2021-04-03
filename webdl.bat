e401da1344e1016b7b40c9c172576835d98c36b4
Paranjay Singh <parnexcodes@gmail.com>
2021-04-03 17:48:31 +0530

    removed path variable

--- webdl.bat
+++ webdl.bat
@@ -7,22 +7,15 @@ echo.
 echo Thanks to u/Own_Western8448 for the original batch script.
 echo.
 
-set /p path=Input directory containing all the script files : 
-
-echo.
-
 set /p folder= Video Name to be Saved as (No spaces) : 
 
 echo.
 
-mkdir %path%\Downloads\%folder%
+mkdir Downloads\%folder%
 
 set /p stream=Input MPD URL : 
 
-yt-dlp --external-downloader aria2c --allow-unplayable-formats -F "%stream%" > formatid.txt
-
-echo.
-echo ID's have been exported to formatid.txt (Open with Notepad++) to check ID's
+yt-dlp --external-downloader aria2c --allow-unplayable-formats -F "%stream%"
 echo.
 
 set /p video=Video Quality ID : 
@@ -33,13 +26,13 @@ set /p audio=Audio Quality ID :
 
 echo.
 
-yt-dlp --external-downloader aria2c --allow-unplayable-formats -f %video%+%audio% "%stream%" -o "%path%\Downloads\%folder%\encrypted.%%(ext)s"
+yt-dlp --external-downloader aria2c --allow-unplayable-formats -f %video%+%audio% "%stream%" -o "Downloads\%folder%\encrypted.%%(ext)s"
 
 echo.
 
-ren "%path%\Downloads\%folder%\*.mp4" encrypted.mp4
+ren "Downloads\%folder%\*.mp4" encrypted.mp4
 
-ren "%path%\Downloads\%folder%\*.m4a" encrypted.m4a
+ren "Downloads\%folder%\*.m4a" encrypted.m4a
 
 set /p key=Input key (1st 'bit'): 
 
@@ -51,12 +44,13 @@ echo.
 
 echo Decrypting and Merging .........
 
-mp4decrypt.exe --key %id%:%key% %path%\Downloads\%folder%\encrypted.m4a %path%\Downloads\%folder%\decrypted.m4a
+mp4decrypt.exe --key %id%:%key% Downloads\%folder%\encrypted.m4a Downloads\%folder%\decrypted.m4a
 
-mp4decrypt.exe --key %id%:%key% %path%\Downloads\%folder%\encrypted.mp4 %path%\Downloads\%folder%\decrypted.mp4
+mp4decrypt.exe --key %id%:%key% Downloads\%folder%\encrypted.mp4 Downloads\%folder%\decrypted.mp4
 
-mkvmerge.exe -o %path%\Downloads\%folder%.mkv %path%\Downloads\%folder%\decrypted.mp4 %path%\Downloads\%folder%\decrypted.m4a
+mkvmerge.exe -o Downloads\%folder%.mkv Downloads\%folder%\decrypted.mp4 Downloads\%folder%\decrypted.m4a
 
+//rmdir "Downloads\%folder%\"
 echo.
 echo Completed .........
 echo.
