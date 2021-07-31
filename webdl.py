2658b9a89d3badf443a4a6da851f996262925166
Paranjay Singh <parnexcodes@gmail.com>
2021-07-31 23:08:09 +0530

    fix rename

--- webdl.py
+++ webdl.py
@@ -45,7 +45,7 @@ else:
     subprocess.run([youtubedlexe, '-k', '--allow-unplayable-formats', '--no-check-certificate', '-f', 'ba', '--fixup', 'never', mpdurl, '-o', 'encrypted.m4a', '--external-downloader', aria2cexe, '--external-downloader-args', '-x 16 -s 16 -k 1M'])
     subprocess.run([youtubedlexe, '-k', '--allow-unplayable-formats', '--no-check-certificate', '-f', 'bv', '--fixup', 'never', mpdurl, '-o', 'encrypted.mp4', '--external-downloader', aria2cexe, '--external-downloader-args', '-x 16 -s 16 -k 1M'])    
 
-os.rename("*.log", "keys.txt")
+os.system("ren *.log keys.txt")
 with open("keys.txt", 'r') as f:
     file = f.readlines()
 
