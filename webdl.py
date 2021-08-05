9d39844175b7f3c980b5c095750fd9320ad6b0a8
parnexcodes <parnexcodes@gmail.com>
2021-08-05 09:22:01 +0530

    Revert "Another method"

--- webdl.py
+++ webdl.py
@@ -1,4 +1,5 @@
 import os
+import json
 import subprocess
 import argparse
 import sys
@@ -6,25 +7,29 @@ import pyfiglet
 from rich import print
 from typing import DefaultDict
 
-print("""[magenta]
-
-██╗    ██╗███████╗██████╗ ██████╗ ██╗         ███████╗ ██████╗██████╗ ██╗██████╗ ████████╗
-██║    ██║██╔════╝██╔══██╗██╔══██╗██║         ██╔════╝██╔════╝██╔══██╗██║██╔══██╗╚══██╔══╝
-██║ █╗ ██║█████╗  ██████╔╝██║  ██║██║         ███████╗██║     ██████╔╝██║██████╔╝   ██║   
-██║███╗██║██╔══╝  ██╔══██╗██║  ██║██║         ╚════██║██║     ██╔══██╗██║██╔═══╝    ██║   
-╚███╔███╔╝███████╗██████╔╝██████╔╝███████╗    ███████║╚██████╗██║  ██║██║██║        ██║   
- ╚══╝╚══╝ ╚══════╝╚═════╝ ╚═════╝ ╚══════╝    ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝        ╚═╝   
-                                                                                          
-[/magenta]""")
-print("by [red]parnex[/red]")
+title = pyfiglet.figlet_format('WEBDL Script', font='slant')
+print(f'[magenta]{title}[/magenta]')
+print("by parnex")
 print("Required files : yt-dlp.exe, mkvmerge.exe, mp4decrypt.exe, aria2c.exe\n")
 
 arguments = argparse.ArgumentParser()
-arguments.add_argument("-m", "--video-link", dest="mpd", help="MPD url", required=True)
+# arguments.add_argument("-m", "--video-link", dest="mpd", help="MPD url")
 arguments.add_argument("-o", '--output', dest="output", help="Specify output file name with no extension", required=True)
 arguments.add_argument("-id", dest="id", action='store_true', help="use if you want to manually enter video and audio id.")
 arguments.add_argument("-s", dest="subtitle", help="enter subtitle url")
 args = arguments.parse_args()
+
+with open("keys.json") as json_data:
+    config = json.load(json_data)
+    json_mpd_url = config[0]['mpd_url']
+    try:
+        keys = ""
+        for i in range(1, len(config)):
+            keys += f"--key {config[i]['kid']}:{config[i]['hex_key']} "
+    except:
+        keys = ""
+        for i in range(1, len(config)-1):
+            keys += f"--key {config[i]['kid']}:{config[i]['hex_key']} "
 
 currentFile = __file__
 realPath = os.path.realpath(currentFile)
@@ -37,62 +42,33 @@ mp4decryptexe = dirPath + '/binaries/mp4decrypt_new.exe'
 mkvmergeexe = dirPath + '/binaries/mkvmerge.exe'
 SubtitleEditexe = dirPath + '/binaries/SubtitleEdit.exe'
 
-mpdurl = str(args.mpd)
+# mpdurl = str(args.mpd)
 output = str(args.output)
 subtitle = str(args.subtitle)
 
 if args.id:
-    subprocess.run([youtubedlexe, '-k', '--allow-unplayable-formats', '--no-check-certificate', '-F', mpdurl])
+    print(f'Selected MPD : {json_mpd_url}\n')    
+    subprocess.run([youtubedlexe, '-k', '--allow-unplayable-formats', '--no-check-certificate', '-F', json_mpd_url])
 
     vid_id = input("\nEnter Video ID : ")
     audio_id = input("Enter Audio ID : ")
-    subprocess.run([youtubedlexe, '-k', '--allow-unplayable-formats', '--no-check-certificate', '-f', audio_id, '--fixup', 'never', mpdurl, '-o', 'encrypted.m4a', '--external-downloader', aria2cexe, '--external-downloader-args', '-x 16 -s 16 -k 1M'])
-    subprocess.run([youtubedlexe, '-k', '--allow-unplayable-formats', '--no-check-certificate', '-f', vid_id, '--fixup', 'never', mpdurl, '-o', 'encrypted.mp4', '--external-downloader', aria2cexe, '--external-downloader-args', '-x 16 -s 16 -k 1M'])
+    subprocess.run([youtubedlexe, '-k', '--allow-unplayable-formats', '--no-check-certificate', '-f', audio_id, '--fixup', 'never', json_mpd_url, '-o', 'encrypted.m4a', '--external-downloader', aria2cexe, '--external-downloader-args', '-x 16 -s 16 -k 1M'])
+    subprocess.run([youtubedlexe, '-k', '--allow-unplayable-formats', '--no-check-certificate', '-f', vid_id, '--fixup', 'never', json_mpd_url, '-o', 'encrypted.mp4', '--external-downloader', aria2cexe, '--external-downloader-args', '-x 16 -s 16 -k 1M'])   
 
 else:
-    subprocess.run([youtubedlexe, '-k', '--allow-unplayable-formats', '--no-check-certificate', '-f', 'ba', '--fixup', 'never', mpdurl, '-o', 'encrypted.m4a', '--external-downloader', aria2cexe, '--external-downloader-args', '-x 16 -s 16 -k 1M'])
-    subprocess.run([youtubedlexe, '-k', '--allow-unplayable-formats', '--no-check-certificate', '-f', 'bv', '--fixup', 'never', mpdurl, '-o', 'encrypted.mp4', '--external-downloader', aria2cexe, '--external-downloader-args', '-x 16 -s 16 -k 1M'])    
+    print(f'Selected MPD : {json_mpd_url}\n')
+    subprocess.run([youtubedlexe, '-k', '--allow-unplayable-formats', '--no-check-certificate', '-f', 'ba', '--fixup', 'never', json_mpd_url, '-o', 'encrypted.m4a', '--external-downloader', aria2cexe, '--external-downloader-args', '-x 16 -s 16 -k 1M'])
+    subprocess.run([youtubedlexe, '-k', '--allow-unplayable-formats', '--no-check-certificate', '-f', 'bv', '--fixup', 'never', json_mpd_url, '-o', 'encrypted.mp4', '--external-downloader', aria2cexe, '--external-downloader-args', '-x 16 -s 16 -k 1M'])    
 
-def getkeys():
-    with open("keys.txt", 'r') as f:
-        file = f.readlines()
-
-    length = len(file)
-
-    keys = ""
-    for i in range(0, length):
-        key = file[i][33 : 65]
-        kid = file[i][0 : 32]
-
-        keys += f'--key {kid}:{key} '
-        return keys
-
-def getkeys1():
-    with open("keys (1).txt", 'r') as f:
-        file = f.readlines()
-
-    length = len(file)
-
-    keys = ""
-    for i in range(0, length):
-        key = file[i][33 : 65]
-        kid = file[i][0 : 32]
-
-        keys += f'--key {kid}:{key} '
-        return keys
 
 print("\nDecrypting .....")
-try:
-    subprocess.run(f'{mp4decryptexe} --show-progress {getkeys()} encrypted.m4a decrypted.m4a', shell=True)
-    subprocess.run(f'{mp4decryptexe} --show-progress {getkeys()} encrypted.mp4 decrypted.mp4', shell=True)
-except:
-    subprocess.run(f'{mp4decryptexe} --show-progress {getkeys1()} encrypted.m4a decrypted.m4a', shell=True)
-    subprocess.run(f'{mp4decryptexe} --show-progress {getkeys1()} encrypted.mp4 decrypted.mp4', shell=True)    
+subprocess.run(f'{mp4decryptexe} --show-progress {keys} encrypted.m4a decrypted.m4a', shell=True)
+subprocess.run(f'{mp4decryptexe} --show-progress {keys} encrypted.mp4 decrypted.mp4', shell=True)  
 
 if args.subtitle:
     subprocess.run(f'{aria2cexe} {subtitle}', shell=True)
-    os.system('ren *.xml en.xml') # Change this to your subtitle extension
-    subprocess.run(f'{SubtitleEditexe} /convert en.xml srt', shell=True) # Change .xml to your extension    
+    os.system('ren *.xml en.xml')
+    subprocess.run(f'{SubtitleEditexe} /convert en.xml srt', shell=True) 
     print("Merging .....")
     subprocess.run([mkvmergeexe, '--ui-language' ,'en', '--output', output +'.mkv', '--language', '0:eng', '--default-track', '0:yes', '--compression', '0:none', 'decrypted.mp4', '--language', '0:eng', '--default-track', '0:yes', '--compression' ,'0:none', 'decrypted.m4a','--language', '0:eng','--track-order', '0:0,1:0,2:0,3:0,4:0', 'en.srt'])
     print("\nAll Done .....")
@@ -109,9 +85,7 @@ if delete_choice == 1:
     os.remove("encrypted.mp4")
     os.remove("decrypted.m4a")
     os.remove("decrypted.mp4")
-    try:
-        os.remove("keys.txt")  
-        os.remove("keys (1).txt")
+    try:    
         os.remove("en.srt")
     except:
         pass
